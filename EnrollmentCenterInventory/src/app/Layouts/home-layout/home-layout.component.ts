e133caba91d28b43952aebaaaba6a9d872a92a42
mlb0066 <mlb0066@mix.wvu.edu>
2023-03-05 16:20:13 -0500

    MB-030523-1620

--- EnrollmentCenterInventory/src/app/Layouts/home-layout/home-layout.component.ts
+++ EnrollmentCenterInventory/src/app/Layouts/home-layout/home-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ItemModel } from 'src/app/Components/LayoutComponents/product-list/item.model'
+import { item_list } from 'src/app/Components/LayoutComponents/product-list/item_list'
 
 @Component({
   selector: 'app-home-layout',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-layout.component.css']
 })
 export class HomeLayoutComponent {
+  items: ItemModel [] = [];
 
+  constructor(){
+    for (var item of item_list){
+      console.log(item);
+      this.items.push(item);
+    }
+  }
 }
