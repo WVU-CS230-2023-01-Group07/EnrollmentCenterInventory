fbebcd68f60a1cb8fe03ae179864876bb2495ac8
mysteriosu <mrt011603@live.com>
2023-04-17 12:40:50 -0400

    e-mail and password login

--- EnrollmentCenterInventory/src/app/Layouts/sign-in-layout/sign-in-layout.component.ts
+++ EnrollmentCenterInventory/src/app/Layouts/sign-in-layout/sign-in-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/Components/Common/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in-layout',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in-layout.component.css']
 })
 export class SignInLayoutComponent {
-
-}
+  /*Basically just passes the signIn method to the sign-in-layout.component.html file, and if the user is logged in, redirects the user to the home page.*/
+  constructor(private authentication: UserService, private router: Router ) {}
+    async signIn(user: HTMLInputElement, pass: HTMLInputElement) {
+      await this.authentication.SignIn(user, pass);
+      if(this.authentication.isAuth())this.router.navigate(['home']);
+    }
+    }
