e19346271bfb525c72a7e3ca38a44a7bb7ee1c83
mlb0066 <mlb0066@mix.wvu.edu>
2023-04-27 18:25:11 -0400

    MB 042723_1825

--- EnrollmentCenterInventory/src/environments/environment.development.ts
+++ EnrollmentCenterInventory/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   firebase: {
     projectId: 'wvu-ec-database',
     appId: '1:822312610806:web:31d6079d4fece09d4cf5d7',
@@ -9,3 +10,26 @@ export const environment = {
     messagingSenderId: '822312610806',
     measurementId: 'G-6JWJ4FNF16',
   },};
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBmw3wWKx2a9JwHyunk-YRl8CoyIVg3g80",
+  authDomain: "wvu-ec-database.firebaseapp.com",
+  databaseURL: "https://wvu-ec-database-default-rtdb.firebaseio.com",
+  projectId: "wvu-ec-database",
+  storageBucket: "wvu-ec-database.appspot.com",
+  messagingSenderId: "822312610806",
+  appId: "1:822312610806:web:b5a02b918a64dde94cf5d7",
+  measurementId: "G-60KEC0YDYL"
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
