02846edcbfc6ee9f65577270b37780d9e7857763
mysteriosu <mrt011603@live.com>
2023-04-28 12:35:07 -0400

    Implemented sign-in error handling
    Also removed mentions of the redundant isLoggedIn var, replaced by cookies.
    
    sign-in page will now use firebaseerror to detect the specific error (wrong email / password, invalid user) and relay that info to the user.

--- EnrollmentCenterInventory/src/app/Components/Common/auth.service.ts
+++ EnrollmentCenterInventory/src/app/Components/Common/auth.service.ts
@@ -1,36 +1,40 @@
 import { Injectable } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { FirebaseError } from "firebase/app";
 
 @Injectable({
     providedIn: 'root',
 })
 
 export class UserService {
-    /*Boolean checks whether logged in or not, this is used for routing - see sign-in-layout.component.ts*/
-    private isLoggedIn = false;
-    constructor(public auth: AngularFireAuth) {}
+    constructor(public auth: AngularFireAuth) { }
     /*HTMLInputElements are what is inputted from the form in the sign in page, we then convert these to strings by getting their value.*/
-    async SignIn(user: HTMLInputElement, pass: HTMLInputElement){
-        try{
+    async SignIn(user: HTMLInputElement, pass: HTMLInputElement) {
+        try {
             const userS: string = user.value;
             const passS: string = pass.value;
             /*Call the signInWithEmailAndPassword method of the AngularFireAuth class, passing in the user and pass as parameters.*/
             const result = await this.auth.signInWithEmailAndPassword(userS, passS);
-            /*If the result is successful, set the isLoggedIn variable to true, and redirect the user to the home page.*/
-            console.log(result);
-            this.isLoggedIn = true;
+            /*If the result is successful, set the loggedIn cookie to true, and redirect the user to the home page.*/
             this.setCookie("loggedIn", "true");
         } catch (error) {
-            /*If there is an error in the sign in process, log the error and ensure isLoggedIn is set to false (again, see sign-in-layout.component.ts for more info) */
-            console.log(error);
-            this.isLoggedIn = false;
+            /*If there is an error in the sign in process, log the error and ensure the loggedIn cookie is set to false. (again, see sign-in-layout.component.ts for more info) */
             this.setCookie("loggedIn", "false");
+            /*Error handling for all of the firebase log-in errors*/
+            if (error instanceof FirebaseError) {
+                if (error.code == "auth/invalid-email")
+                    alert("Ensure you're using a valid email address. Please try again.");
+                if (error.code == "auth/user-not-found")
+                    alert("Sorry! The user you're trying to log in as could not be found. Please double check your email address and password, and try again.");
+                if (error.code == "auth/wrong-password")
+                    alert("Sorry! Your password is incorrect. Please try again.");
+            }
         }
     }
 
 
-    async SignUp(user: HTMLInputElement, pass: HTMLInputElement){
-        try{
+    async SignUp(user: HTMLInputElement, pass: HTMLInputElement) {
+        try {
             const userS: string = user.value;
             const passS: string = pass.value;
             /*Call the createUserWithEmailAndPassword method of the AngularFireAuth class, passing in the user and pass as parameters.*/
@@ -39,16 +43,14 @@ export class UserService {
             console.log(result);
             alert("Sign up successful!");
         } catch (error) {
-            /*If there is an error in the sign in process, log the error and ensure isLoggedIn is set to false (again, see sign-in-layout.component.ts for more info) */
+            /*If there is an error in the sign in process, log the error and alert the user.*/
             console.log(error);
-            alert("Sorry! Your sign up was unsuccessful. Please try again.");
         }
     }
 
     //Signs the user out
-    async signOut(){
-        try{
-            this.isLoggedIn = false;
+    async signOut() {
+        try {
             this.setCookie("loggedIn", "false");
 
         } catch (error) {
@@ -57,14 +59,13 @@ export class UserService {
         }
     }
 
-    public isAuth(){
-        //return this.isLoggedIn <- Old way
+    public isAuth() {
         console.log("cookie: " + this.getCookie("loggedIn"))
-        
-        if(this.getCookie("loggedIn") == "true"){
+
+        if (this.getCookie("loggedIn") == "true") {
             return true;
         }
-        
+
         return false;
     }
 
@@ -76,13 +77,13 @@ export class UserService {
         date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000));
 
         // Set the cookie
-        document.cookie = name+"="+value+"; expires="+date.toUTCString()+"; path=/";
+        document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
     }
 
     private getCookie(name: string) {
         const value = "; " + document.cookie;
         const parts = value.split("; " + name + "=");
-        
+
         //return the cookie's value
         if (parts.length == 2) {
             return parts.pop()?.split(";").shift();
